88d15fcfccd26a4ab63aa1043fee6407d02082ed
Sagar2366 <sagarutekar2366@gmail.com>
2018-09-23 14:44:04 +0530

    new fix

--- scripts/dockercommands.sh
+++ scripts/dockercommands.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 docker build -t sampleapp1 .
 docker run -p 4378:4378 -d sampleapp1
+git add .
+git commit -m "after docker operations"
+git push origin master -u "Sagar2366" -p "Utekar@2366"
 docker ps -a
