c6dcef111b82ea72351e64db6175a2b395185e88
Sagar2366 <sagarutekar2366@gmail.com>
2018-09-23 15:35:04 +0530

    new fix

--- scripts/dockercommands.sh
+++ scripts/dockercommands.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-docker build -t sampleapp1 .
+# docker build -t sampleapp1 .
+# docker run -p 4378:4378 -d sampleapp1
+# # git add .
+# # git commit -m "after docker operations"
+# # git push origin master -u "Sagar2366" -p "Utekar@2366"
+# docker ps -a
+
+docker login docker.artifactory -u "admin" -p "Utekar@2366"
+docker tag sampleapp1 docker.artifactory/sampleapp1
+docker images
 docker run -p 4378:4378 -d sampleapp1
-# git add .
-# git commit -m "after docker operations"
-# git push origin master -u "Sagar2366" -p "Utekar@2366"
 docker ps -a
