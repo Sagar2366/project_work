4509fd36b3892a03f38439b0079b89e8f4d6bac7
Sagar2366 <sagarutekar2366@gmail.com>
2018-09-23 15:08:43 +0530

    new fix

--- scripts/dockercommands.sh
+++ scripts/dockercommands.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 docker build -t sampleapp1 .
-docker run -p 127.0.0.1:4378:4378 -d sampleapp1 --network "host" bash 
-curl -i 127.0.0.1:4378
-curl -i localhost:4378
+docker run -p 4378:4378 -d sampleapp1
 # git add .
 # git commit -m "after docker operations"
 # git push origin master -u "Sagar2366" -p "Utekar@2366"
