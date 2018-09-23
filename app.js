ef113ba69004f195a15584ebee02ea5e6ed17279
Sagar2366 <sagarutekar2366@gmail.com>
2018-09-23 13:59:11 +0530

    new fix

--- app.js
+++ app.js
@@ -4,15 +4,4 @@ http.createServer((req,res) => {
     res.write("Hi successfully running your first application !")
 }).listen(4378, () => {
     console.log('running at port 4378')
-    console.log('running at port 4378')
-});
-
-
-const http = require('http')
-
-http.createServer((req,res) => {
-    res.write("Hi successfully running your first application !")
-}).listen(4378, () => {
-    console.log('running at port 4378')
-    console.log('running at port 4378')
 });
