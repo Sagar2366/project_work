aa23c320188116038f46fbbfba79a2dbad7fa21f
Sagar2366 <sagarutekar2366@gmail.com>
2018-09-24 15:27:23 +0530

    new commit

--- scripts/dockercommands.sh
+++ scripts/dockercommands.sh
@@ -5,12 +5,14 @@
 # # git commit -m "after docker operations"
 # # git push origin master -u "Sagar2366" -p "Utekar@2366"
 # docker ps -a
-docker build -t sampleapp1 .
-docker pull hello-world
-docker login docker.artifactory -u "admin" -p "Utekar@2366"
-docker tag hello-world docker.artifactory/hello-world
-#docker tag sampleapp1 docker.artifactory/sampleapp1
-docker push docker.artifactory/hello-world
-docker images
-docker run -p 4378:4378 -d sampleapp1
-docker ps -a
+# docker build -t sampleapp1 .
+# docker pull hello-world
+# docker login docker.artifactory -u "admin" -p "Utekar@2366"
+# docker tag hello-world docker.artifactory/hello-world
+# #docker tag sampleapp1 docker.artifactory/sampleapp1
+# docker push docker.artifactory/hello-world
+# docker images
+# docker run -p 4378:4378 -d sampleapp1
+# docker ps -a
+
+curl -fL https://getcli.jfrog.io | sh
