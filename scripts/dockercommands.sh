7e68dbf70e9b7560e2718af08c90395013480f1a
Sagar2366 <sagarutekar2366@gmail.com>
2018-09-24 13:30:17 +0530

    new commit

--- scripts/dockercommands.sh
+++ scripts/dockercommands.sh
@@ -6,9 +6,11 @@
 # # git push origin master -u "Sagar2366" -p "Utekar@2366"
 # docker ps -a
 docker build -t sampleapp1 .
+docker pull hello-world
 docker login docker.artifactory -u "admin" -p "Utekar@2366"
 docker tag hello-world docker.artifactory/hello-world
-docker tag sampleapp1 docker.artifactory/sampleapp1
+#docker tag sampleapp1 docker.artifactory/sampleapp1
+docker push docker.artifactory/hello-world
 docker images
 docker run -p 4378:4378 -d sampleapp1
 docker ps -a
