9e398e1d6c9b753d578deaa646fb4ac863c0a2a4
Sagar2366 <sagarutekar2366@gmail.com>
2018-09-25 05:24:19 +0530

    new message

--- scripts/dockercommands.sh
+++ scripts/dockercommands.sh
@@ -22,6 +22,28 @@
 pwd
 ls -al
 ifconfig
-ping -c 2 10.136.60.50
+# ping -c 2 10.136.60.
+
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo npm install npm@latest -g
+sudo apt-get install -y build-essential
+sudo npm install -g -unsafe-perm jfrog-cli-go
+jfrog
 #curl -u admin:Utekar@2366 -X PUT "http://192.168.31.182/artifactory/docker-local/file.txt" -T file1.txt
-curl -u admin:admin -X PUT "http://10.136.60.50/artifactory/docker-local/file.txt" -T file.txt
+# curl -u admin:admin -X PUT "http://10.136.60.50/artifactory/docker-local/file.txt" -T file.
+
+# sudo jfrog rt c rt-server-1 --url=http://10.136.60.50/artifactory/ --user=admin --password=admin
+# [Info] Encrypting password...
+
+# (ubuntu@ubuntu)[~/.jfrog]sudo chmod 777 jfrog-cli.conf 
+
+# (ubuntu@ubuntu)[/]jfrog rt u file.txt docker-local/file.txt
+# [Info] [Thread 2] Uploading artifact: file.txt
+# {
+#   "status": "success",
+#   "totals": {
+#     "success": 1,
+#     "failure": 0
+#   }
+# }
