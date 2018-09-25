0b22c9a926410a4e38e72fe7047c3220865277aa
Sagar2366 <sagarutekar2366@gmail.com>
2018-09-25 06:31:25 +0530

    new message

--- scripts/dockercommands.sh
+++ scripts/dockercommands.sh
@@ -30,7 +30,7 @@ sudo npm install npm@latest -g
 sudo apt-get install -y build-essential
 sudo npm install -g -unsafe-perm jfrog-cli-go
 jfrog
-sudo export JFROG_CLI_HOME="."
+sudo JFROG_CLI_HOME="."
 sudo chmod 777 ./.jfrog/jfrog-cli.conf
 #curl -u admin:Utekar@2366 -X PUT "http://192.168.31.182/artifactory/docker-local/file.txt" -T file1.txt
 #curl -u admin:admin -X PUT "http://10.136.60.50/artifactory/docker-local/somefile.txt" -T somefile.txt
