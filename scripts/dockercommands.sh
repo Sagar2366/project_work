bec35a206d640b97648501b5874f44919e23893d
Sagar2366 <sagarutekar2366@gmail.com>
2018-09-25 07:29:13 +0530

    new message

--- scripts/dockercommands.sh
+++ scripts/dockercommands.sh
@@ -43,7 +43,7 @@ ls -al
 # cat jfrog-cli.conf
 sudo jfrog rt c rt-server-6 --url=http://10.136.60.50/artifactory/ --user=admin --password=admin
 # [Info] Encrypting password...
-chmod 777 ~/.jfrog/jfrog-cli.conf
+chmod 777 /home/travis/build/Sagar2366/project_work/.jfrog/jfrog-cli.conf
 ls -al
 # chmod 777 ~/.jfrog/jfrog-cli.conf
 # ls -al
